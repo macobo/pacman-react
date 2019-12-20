1efc2737f2f4ab5a76d6bd7f00fbe8e7d1cf0435
Sander Siim <sander.siim1@gmail.com>
2019-12-20 11:58:15 +0200

    Fix indentation

--- src/scripts/pacmanApp.jsx
+++ src/scripts/pacmanApp.jsx
@@ -3,31 +3,26 @@ import Scene from './components/Scene/scene.jsx';
 import Header from './components/Header/header.jsx';
 
 class PacmanApp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
+  increaseValue() {
+    this.refs.header.increase();
+  }
 
-    increaseValue() {
-        this.refs.header.increase();
-    }
+  gameOver() {
+    this.refs.header.gameOver();
+  }
 
-    gameOver() {
-        this.refs.header.gameOver();
-    }
-
-    render() {
-        return (
-            <div className="pacman-app">
-                <Header ref="header"></Header>
-                <Scene gameOver = {this.gameOver.bind(this)} increase= {this.increaseValue.bind(this)}></Scene>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="pacman-app">
+        <Header ref="header"></Header>
+        <Scene gameOver = {this.gameOver.bind(this)} increase= {this.increaseValue.bind(this)}></Scene>
+      </div>
+    );
+  }
 }
-
-PacmanApp.defaultProps = {
-
-};
 
 export default PacmanApp;
